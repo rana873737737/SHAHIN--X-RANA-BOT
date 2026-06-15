fc7dcbb990f3450ab2086e9a3d4fa37a95972a0e
rana873737737 <shahinevuu@gmail.com>
2026-06-15 21:01:10 +0600

    Update demote.js

--- grpUpCmd/demote.js
+++ grpUpCmd/demote.js
@@ -2,11 +2,13 @@ module.exports = {
   event: 'demote',
   handle: async ({ api, event }) => {
     const demotedMembers = event.participants;
-    console.log(event);
+    const adminWhoDemoted = event.author; // কে ডিমোট করলো
+
     for (const member of demotedMembers) {
       await api.sendMessage(event.id, {
-        text: `😢 @${member.split('@')[0]} has been demoted. Better luck next time!`,
-        mentions: [member]
+        text: `😢 @${member.split('@')[0]} ⎯͢✧ 𝐡ᴀꜱ ʙᴇᴇɴ ᴅᴇᴍᴏᴛᴇᴅ. 𝐁ᴇᴛᴛᴇʀ ʟᴜᴄᴋ ɴᴇxᴛ ᴛɪᴍᴇ!
+👮‍♂️⎯͢✧𝐃ᴇᴍᴏᴛᴇᴅ 𝐁ʏ: @${adminWhoDemoted.split('@')[0]}`,
+        mentions: [member, adminWhoDemoted]
       });
     }
   }
